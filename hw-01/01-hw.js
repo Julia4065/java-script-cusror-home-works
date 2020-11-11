9b7e9a5084f89cc6f3f88db560712c3eb8a39527
julia <julia.k@stampli.com>
2020-11-11 23:42:47 +0200

    home-work-1

---
+++ hw-01/01-hw.js
@@ -0,0 +1,54 @@
+let tomatoPrice = 15.678;
+let cucumberPrice = 123.965;
+let orangePrice = 90.2345;
+
+
+let productsMaxPrice = Math.max(tomatoPrice, cucumberPrice, orangePrice);
+console.log(`Max price is: ${productsMaxPrice}`);
+
+let productsMinPrice = Math.min(tomatoPrice, cucumberPrice, orangePrice);
+console.log(`Min price is: ${productsMinPrice}`);
+
+let totalSumOfAllProducts = tomatoPrice + cucumberPrice + orangePrice;
+console.log(`Total sum of all products is: ${totalSumOfAllProducts}`);
+
+
+let tomatoPriceIntegerPart = Math.trunc(tomatoPrice);
+let cucumberPriceIntegerPart = Math.trunc(cucumberPrice);
+let orangePriceIntegerPart = Math.trunc(orangePrice);
+
+let totalSumOfIntegerPartsForProducts = Math.floor(tomatoPriceIntegerPart + cucumberPriceIntegerPart + orangePriceIntegerPart);
+console.log(`Total sum of all products (only integer parts) is: ${totalSumOfIntegerPartsForProducts}`);
+
+let roundedTotalSum = Math.ceil(totalSumOfIntegerPartsForProducts / 100) * 100;
+console.log(`Total sum (rounded up to hundreds) is: ${roundedTotalSum}`);
+
+let isSumEven = undefined;
+if ((totalSumOfIntegerPartsForProducts % 2) == 0) {
+	isSumEven = true;
+} else {
+	isSumEven = false;
+}
+console.log(`Sum of all products (only integer parts) is even: ${isSumEven}`);
+
+let remainderAfterPayment = 500 - totalSumOfAllProducts;
+console.log(`Remainder after 500 grn were paid is: ${remainderAfterPayment}`);
+
+let averageValueOfProductPrices = (totalSumOfAllProducts/3).toFixed(2);
+console.log(`Average value of products prices is: ${averageValueOfProductPrices}`);
+
+
+let randomDiscountRate = Math.floor(Math.random() * 100) + 1;
+console.log(`Discount is ${randomDiscountRate}`);
+
+let discountValue = Math.floor((totalSumOfAllProducts * randomDiscountRate) / 100);
+console.log(`Discount value is ${discountValue}`);
+
+let sumToPayWithDiscount = (totalSumOfAllProducts - discountValue).toFixed(2);
+console.log(`Sum to pay (with discount) is ${sumToPayWithDiscount}`);
+
+let costPrice = Math.trunc(totalSumOfAllProducts / 2);
+console.log(`Cost price is ${costPrice}`);
+
+let netProfit = costPrice - discountValue;
+console.log(`Net profit is ${netProfit}`);
