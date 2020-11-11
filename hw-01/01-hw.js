e1a002cc94738b266b35ab02c29b807ba5e95cce
julia <julia.k@stampli.com>
2020-11-12 00:18:06 +0200

    make almost all constants

--- hw-01/01-hw.js
+++ hw-01/01-hw.js
@@ -2,26 +2,26 @@ const tomatoPrice = 15.678;
 const cucumberPrice = 123.965;
 const orangePrice = 90.2345;
 
-let usersAmount = 500;
+const usersAmount = 500;
 
-let productsMaxPrice = Math.max(tomatoPrice, cucumberPrice, orangePrice);
+const productsMaxPrice = Math.max(tomatoPrice, cucumberPrice, orangePrice);
 console.log(`Max price is: ${productsMaxPrice}`);
 
-let productsMinPrice = Math.min(tomatoPrice, cucumberPrice, orangePrice);
+const productsMinPrice = Math.min(tomatoPrice, cucumberPrice, orangePrice);
 console.log(`Min price is: ${productsMinPrice}`);
 
-let totalSumOfAllProducts = tomatoPrice + cucumberPrice + orangePrice;
+const totalSumOfAllProducts = tomatoPrice + cucumberPrice + orangePrice;
 console.log(`Total sum of all products is: ${totalSumOfAllProducts}`);
 
 
-let tomatoPriceIntegerPart = Math.trunc(tomatoPrice);
-let cucumberPriceIntegerPart = Math.trunc(cucumberPrice);
-let orangePriceIntegerPart = Math.trunc(orangePrice);
+const tomatoPriceIntegerPart = Math.trunc(tomatoPrice);
+const cucumberPriceIntegerPart = Math.trunc(cucumberPrice);
+const orangePriceIntegerPart = Math.trunc(orangePrice);
 
-let totalSumOfIntegerPartsForProducts = Math.floor(tomatoPriceIntegerPart + cucumberPriceIntegerPart + orangePriceIntegerPart);
+const totalSumOfIntegerPartsForProducts = Math.floor(tomatoPriceIntegerPart + cucumberPriceIntegerPart + orangePriceIntegerPart);
 console.log(`Total sum of all products (only integer parts) is: ${totalSumOfIntegerPartsForProducts}`);
 
-let roundedTotalSum = Math.ceil(totalSumOfIntegerPartsForProducts / 100) * 100;
+const roundedTotalSum = Math.ceil(totalSumOfIntegerPartsForProducts / 100) * 100;
 console.log(`Total sum (rounded up to hundreds) is: ${roundedTotalSum}`);
 
 let isSumEven = undefined;
@@ -32,24 +32,24 @@ if ((totalSumOfIntegerPartsForProducts % 2) == 0) {
 }
 console.log(`Sum of all products (only integer parts) is even: ${isSumEven}`);
 
-let remainderAfterPayment = usersAmount - totalSumOfAllProducts;
+const remainderAfterPayment = usersAmount - totalSumOfAllProducts;
 console.log(`Remainder after 500 grn were paid is: ${remainderAfterPayment}`);
 
-let averageValueOfProductPrices = parseFloat((totalSumOfAllProducts/3).toFixed(2));
+const averageValueOfProductPrices = parseFloat((totalSumOfAllProducts/3).toFixed(2));
 console.log(`Average value of products prices is: ${averageValueOfProductPrices}`);
 
 
-let randomDiscountRate = Math.floor(Math.random() * 100) + 1;
+const randomDiscountRate = Math.floor(Math.random() * 100) + 1;
 console.log(`Discount is ${randomDiscountRate}`);
 
-let discountValue = Math.floor((totalSumOfAllProducts * randomDiscountRate) / 100);
+const discountValue = Math.floor((totalSumOfAllProducts * randomDiscountRate) / 100);
 console.log(`Discount value is ${discountValue}`);
 
-let sumToPayWithDiscount = parseFloat((totalSumOfAllProducts - discountValue).toFixed(2));
+const sumToPayWithDiscount = parseFloat((totalSumOfAllProducts - discountValue).toFixed(2));
 console.log(`Sum to pay (with discount) is ${sumToPayWithDiscount}`);
 
-let costPrice = Math.trunc(totalSumOfAllProducts / 2);
+const costPrice = Math.trunc(totalSumOfAllProducts / 2);
 console.log(`Cost price is ${costPrice}`);
 
-let netProfit = costPrice - discountValue;
+const netProfit = costPrice - discountValue;
 console.log(`Net profit is ${netProfit}`);
