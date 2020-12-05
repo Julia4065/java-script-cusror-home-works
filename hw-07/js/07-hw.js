1851989c2d43a1fe6b23e76f02438098f2f01f0f
julia <julia.k@stampli.com>
2020-12-06 01:12:19 +0200

    Home work 7 - convert results  to numbers

--- hw-07/js/07-hw.js
+++ hw-07/js/07-hw.js
@@ -3,27 +3,27 @@ const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
 function getMyTaxes(salary) {
-    return salary * this.tax;
+    return +(salary * this.tax);
 }
 
 
 function getMiddleTaxes() {
-    return (this.tax * this.middleSalary).toFixed(2);
+    return +(this.tax * this.middleSalary).toFixed(2);
 }
 
 
 function getTotalTaxes() {
-    return (this.tax * this.middleSalary * this.vacancies);
+    return +(this.tax * this.middleSalary * this.vacancies);
 }
 
 
 function getMySalary(salaryFrom, salaryTo) {
-    const salary = salaryFrom + Math.floor((salaryTo - salaryFrom) * Math.random());
-    const taxes = (this.tax * salary).toFixed(2);
+    const salary = +(salaryFrom + Math.floor((salaryTo - salaryFrom) * Math.random()));
+    const taxes = +(this.tax * salary).toFixed(2);
     return {
         salary,
         taxes,
-        profit: (salary - taxes).toFixed(2)
+        profit: +(salary - taxes).toFixed(2)
     }
 }
 
