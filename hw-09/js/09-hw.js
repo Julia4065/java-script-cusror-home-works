3e75602250dff5c8be3ce76cd561defbbc45b92b
julia <julia.k@stampli.com>
2020-12-13 14:16:27 +0200

    Home work 9

---
+++ hw-09/js/09-hw.js
@@ -0,0 +1,24 @@
+getRandomColor = () => {
+    return '#' + Math.random().toString(16).substr(-6);
+}
+
+generateBlocks = () => {
+
+    const allSquaresArea = document.querySelector(".squares");
+    allSquaresArea.innerHTML = ' ';
+
+    for (let i = 0; i < 25; i++) {
+        const square = document.createElement('div');
+        square.style.width = '50px';
+        square.style.height = '50px';
+        square.style.backgroundColor = getRandomColor();
+
+        allSquaresArea.append(square);
+    }
+}
+
+generateBlocksInterval = () => {
+    setInterval(generateBlocks, 1000);
+}
+
+generateBlocksInterval();
